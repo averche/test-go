35c99d678f6a2cae0104dea6b53059f4c35e3d44
Anton Averchenkov <anton.averchenkov@hashicorp.com>
2022-04-18 17:46:44 -0400

    stuff

--- main.go
+++ main.go
@@ -7,17 +7,21 @@ import (
 	"time"
 )
 
+type requestID int
+
 func main() {
 	ctx := context.Background()
 	ctx, cancel := context.WithDeadline(ctx, time.Now().Add(1*time.Second))
 	defer cancel()
 
-	ctx = context.WithValue(ctx, "a", "asd")
+	ctx = context.WithValue(ctx, requestID(42), "asd")
 
 	sleepAndSay(ctx, 5*time.Second, "hello")
 }
 
 func sleepAndSay(ctx context.Context, sleepTime time.Duration, msg string) {
+	log.Println(ctx.Value(requestID(42)))
+
 	select {
 	case <-time.After(sleepTime):
 		fmt.Println(msg)
