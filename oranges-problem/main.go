fb7f98ef88206c1f7b7ded313b1edd6f7ea01ebe
Anton <anton.averchenkov@hashicorp.com>
2022-12-29 05:35:39 -0500

    move monte carlo in

--- oranges-problem/main.go
+++ oranges-problem/main.go
@@ -77,3 +77,66 @@ func buildTree(a int, b int, prob float64, sum *float64, count *float64, memo ma
 	buildTree(a-1, b, prob/2, sum, count, memo)
 	buildTree(a, b-1, prob/2, sum, count, memo)
 }
+
+// expectedOrangesMonteCarlo runs at most iterationsMax experiments and retuns the average estimate.
+// The function returns early if estimates convege within epsilon.
+func expectedOrangesMonteCarlo(iterationsMax uint64, capacity uint64, epsilon float64) (uint64, float64) {
+	var (
+		total uint64
+
+		// circular array of 10 latest estimates (computed every 1000 iterations)
+		latestEstimates   [10]float64
+		latestEstimateIdx int
+	)
+
+	withinEpsilon := func(latestEstimates []float64, latestEstimateIdx int, epsilon float64) bool {
+		for i := 0; i < len(latestEstimates); i++ {
+			if math.Abs(latestEstimates[i]-latestEstimates[latestEstimateIdx]) > epsilon {
+				return false
+			}
+		}
+		return true
+	}
+
+	for i := uint64(0); i < iterationsMax; i++ {
+		total += calculateOne(
+			time.Now().UnixNano()+int64(i),
+			capacity,
+		)
+
+		if i%1000 == 0 {
+			latest := float64(total) / float64(i+1)
+			latestEstimates[latestEstimateIdx] = latest
+			latestEstimateIdx = (latestEstimateIdx + 1) % 10
+
+			if withinEpsilon(latestEstimates[:], latestEstimateIdx, epsilon) {
+				return i, latest
+			}
+		}
+	}
+
+	return iterationsMax, float64(total) / float64(iterationsMax)
+}
+
+// calculateOne runs a single calculation for the given number of oranges in each box (capacity)
+func calculateOne(seed int64, capacity uint64) uint64 {
+	// intialice both to the given capacity
+	oranges1, oranges2 := capacity, capacity
+
+	rng := rand.New(rand.NewSource(seed))
+
+	for {
+		if rng.Uint64()%2 == 0 {
+			oranges1--
+		} else {
+			oranges2--
+		}
+
+		if oranges1 == 0 {
+			return oranges2
+		}
+		if oranges2 == 0 {
+			return oranges1
+		}
+	}
+}
