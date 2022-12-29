3672d9b18a0e6b45e7d1c979358d3acdf1f58632
Anton Averchenkov <anton.averchenkov@hashicorp.com>
2022-12-28 22:07:12 -0500

    Progressive estimates

--- oranges-monte-carlo/main.go
+++ oranges-monte-carlo/main.go
@@ -2,15 +2,67 @@ package main
 
 import (
 	"fmt"
+	"math"
 	"math/rand"
 	"time"
 )
 
 func main() {
-	fmt.Println(calculateOne(time.Now().UnixNano(), 30))
+	for _, capacity := range []uint64{
+		30,
+		100,
+		1000,
+		10000,
+	} {
+		iterations, estimate := monteCarlo(100000000, capacity, 0.001)
+
+		fmt.Printf(
+			"capacity: %-9d, operations: %-9d, estimate: %f\n",
+			capacity,
+			iterations,
+			estimate,
+		)
+	}
 }
 
-func calculateOne(seed int64, capacity uint32) uint32 {
+func monteCarlo(iterationsMax uint64, capacity uint64, epsilon float64) (uint64, float64) {
+	var (
+		total             uint64
+		latestEstimates   [10]float64
+		latestEstimateIdx int
+	)
+
+	withinEpsilon := func(estimates []float64, epsilon float64) bool {
+		for i := 1; i < len(estimates); i++ {
+			if math.Abs(estimates[i]-estimates[i-1]) > epsilon {
+				return false
+			}
+		}
+		return true
+	}
+
+	for i := uint64(0); i < iterationsMax; i++ {
+		total += calculateOne(
+			time.Now().UnixNano()+int64(i),
+			capacity,
+		)
+
+		if i%1000 == 0 {
+			latest := float64(total) / float64(i+1)
+			latestEstimates[latestEstimateIdx] = latest
+			latestEstimateIdx = (latestEstimateIdx + 1) % 10
+
+			// if the last 3 estimates are within epsilon, return early
+			if withinEpsilon(latestEstimates[:], epsilon) {
+				return i, latest
+			}
+		}
+	}
+
+	return iterationsMax, float64(total) / float64(iterationsMax)
+}
+
+func calculateOne(seed int64, capacity uint64) uint64 {
 	// intialice both to the given capacity
 	oranges1, oranges2 := capacity, capacity
 
