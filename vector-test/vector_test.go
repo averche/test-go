cc1eb96110a023268ede5007a6afba151510e68e
Anton Averchenkov <anton.averchenkov@hashicorp.com>
2022-12-30 00:25:40 -0500

    Add readme with results

--- vector-test/vector_test.go
+++ vector-test/vector_test.go
@@ -1,25 +1,28 @@
 package main
 
-import "testing"
+import (
+	"testing"
+)
 
 var (
 	testData = []Vector3D{
-		{1000.123, 2000.234, 3000.345},
-		{2000.123, 3000.234, 7000.345},
-		{3000.123, 4000.234, 8000.345},
-		{4000.123, 5000.234, 9000.345},
-		{5000.123, 6000.234, 1000.345},
-		{-100.123, -200.234, 3000.345},
-		{-200.123, -300.234, 7000.345},
-		{-300.123, -400.234, 8000.345},
-		{-400.123, -500.234, 9000.345},
-		{-500.123, -600.234, 1000.345},
+		{1000.124, 2000.234, 3000.345},
+		{2000.124, 3000.234, 7000.345},
+		{3000.124, 4000.234, 8000.345},
+		{4000.124, 5000.234, 9000.345},
+		{5000.124, 6000.234, 1000.345},
+		{-100.124, -200.234, 3000.345},
+		{-200.124, -300.234, 7000.345},
+		{-300.124, -400.234, 8000.345},
+		{-400.124, -500.234, 9000.345},
+		{-500.124, -600.234, 1000.345},
 	}
 )
 
 func BenchmarkAdd(b *testing.B) {
+	var vector Vector3D
+
 	for i := 0; i < b.N; i++ {
-		vector := testData[0]
 		for _, v := range testData {
 			vector.Add(&v)
 		}
@@ -27,17 +30,19 @@ func BenchmarkAdd(b *testing.B) {
 }
 
 func BenchmarkAddByValue(b *testing.B) {
+	var vector Vector3D
+
 	for i := 0; i < b.N; i++ {
-		vector := testData[0]
 		for _, v := range testData {
 			vector.AddByValue(v)
 		}
 	}
 }
 
-func BenchmarkAddRetunCopy(b *testing.B) {
+func BenchmarkAddReturnCopy(b *testing.B) {
+	var vector Vector3D
+
 	for i := 0; i < b.N; i++ {
-		vector := testData[0]
 		for _, v := range testData {
 			vector = AddReturnCopy(vector, v)
 		}
