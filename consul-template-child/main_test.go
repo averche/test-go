b9d577e2c17332bdd4fa3ed129943f9d6cc984ef
Anton Averchenkov <84287187+averche@users.noreply.github.com>
2023-05-25 12:50:51 -0400

    consul-template tests: long run (#8)
    * consul-template tests: long run
    
    * readme

--- consul-template-child/main_test.go
+++ consul-template-child/main_test.go
@@ -1,17 +1,18 @@
 package main
 
 import (
+	"fmt"
 	"testing"
 
 	"golang.org/x/sync/errgroup"
 )
 
-func TestRun(t *testing.T) {
+func TestShortRun(t *testing.T) {
 	g := &errgroup.Group{}
 
 	for i := 0; i < 100; i++ {
 		g.Go(func() error {
-			_, err := run()
+			_, err := shortRun()
 			return err
 		})
 	}
@@ -19,5 +20,25 @@ func TestRun(t *testing.T) {
 	if err := g.Wait(); err != nil {
 		t.Fatal(err)
 	}
+}
 
+func TestLongRun(t *testing.T) {
+	g := &errgroup.Group{}
+
+	for i := 0; i < 100; i++ {
+		g.Go(func() error {
+			c, err := longRun()
+			if err != nil {
+				return err
+			}
+			if c != WeStoppedTheProcess {
+				return fmt.Errorf("unexpected return code: %d", c)
+			}
+			return nil
+		})
+	}
+
+	if err := g.Wait(); err != nil {
+		t.Fatal(err)
+	}
 }
