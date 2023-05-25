3d0519e8a0098d79950a20168ee2eb02cb9139c8
Anton Averchenkov <anton.averchenkov@hashicorp.com>
2023-05-25 14:05:39 -0400

    cleanup

--- consul-template-child/main.go
+++ consul-template-child/main.go
@@ -11,10 +11,8 @@ import (
 	"github.com/hashicorp/consul-template/child"
 )
 
-const WeStoppedTheProcess = -0xC0FFEE
-
 func main() {
-	code, err := shortRun()
+	code, err := run()
 
 	if err != nil {
 		log.Fatal(err)
@@ -23,7 +21,7 @@ func main() {
 	log.Println("exit code:", code)
 }
 
-func shortRun() (int, error) {
+func run() (int, error) {
 	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
 	defer cancel()
 
@@ -41,40 +39,6 @@ func shortRun() (int, error) {
 	select {
 	case <-ctx.Done():
 		return -1, fmt.Errorf("did not get an exit code after 5 seconds")
-
-	case exitCode := <-proc.ExitCh():
-		return exitCode, nil
-	}
-}
-
-func longRun() (int, error) {
-	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
-	defer cancel()
-
-	proc, err := child.New(&child.NewInput{
-		Command:      "./my-script-long.sh",
-		Stdin:        os.Stdin,
-		Stdout:       os.Stdout,
-		Stderr:       os.Stderr,
-		Timeout:      0, // let it run forever
-		Env:          os.Environ(),
-		ReloadSignal: nil, // can't reload w/ new env vars
-		KillSignal:   syscall.SIGTERM,
-		KillTimeout:  10 * time.Second,
-		Splay:        0,
-	})
-	if err != nil {
-		return -1, err
-	}
-
-	if err := proc.Start(); err != nil {
-		return -1, fmt.Errorf("could not start proc: %s", err)
-	}
-
-	select {
-	case <-ctx.Done():
-		proc.Stop()
-		return WeStoppedTheProcess, nil
 
 	case exitCode := <-proc.ExitCh():
 		return exitCode, nil
--- consul-template-child/main_test.go
+++ consul-template-child/main_test.go
@@ -1,40 +1,18 @@
 package main
 
 import (
-	"fmt"
 	"testing"
 
 	"golang.org/x/sync/errgroup"
 )
 
-func TestShortRun(t *testing.T) {
+func TestRun(t *testing.T) {
 	g := &errgroup.Group{}
 
 	for i := 0; i < 100; i++ {
 		g.Go(func() error {
-			_, err := shortRun()
+			_, err := run()
 			return err
-		})
-	}
-
-	if err := g.Wait(); err != nil {
-		t.Fatal(err)
-	}
-}
-
-func TestLongRun(t *testing.T) {
-	g := &errgroup.Group{}
-
-	for i := 0; i < 100; i++ {
-		g.Go(func() error {
-			c, err := longRun()
-			if err != nil {
-				return err
-			}
-			if c != WeStoppedTheProcess {
-				return fmt.Errorf("unexpected return code: %d", c)
-			}
-			return nil
 		})
 	}
 
